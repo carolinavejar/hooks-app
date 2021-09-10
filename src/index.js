2f25c348e040338dd0894a79a3118acbaca84689
Carolina Vejar <vejarcarolina.95@gmail.com>
2021-09-09 23:50:40 -0300

    Clase 105 useState

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HooksApp } from './HooksApp';
+import { CounterApp } from './components/01-useState/CounterApp';
+// import { HooksApp } from './HooksApp';
 
 ReactDOM.render(
-    <HooksApp />,
+    <CounterApp />,
   document.getElementById('root')
 );
 

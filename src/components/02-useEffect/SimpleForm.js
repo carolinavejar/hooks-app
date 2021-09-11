6b22bd42e8544db174361cc9c5b51626b9d01670
Carolina Vejar <vejarcarolina.95@gmail.com>
2021-09-10 21:50:58 -0300

    Clase 107 useEffect - SimpleForm

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        name : '',
+        email: ''
+    });
+
+    const {name, email} = formState;
+
+    useEffect(() => {
+        console.log(`hey`);
+    }, []);
+
+    useEffect(() => {
+        console.log(`cambio formulario`);
+    }, [email]);
+
+    const handleInputChange = ({ target })=>{
+        setFormState({
+            ...formState,
+            [ target.name ]:  target.value
+        })
+    }
+    return (
+        <>
+            <h1>useEffect</h1>
+
+            <hr/>
+            <div className="form-group">
+                <input
+                    type="text"
+                    name="name"
+                    className="form-class"
+                    placeholder="Tu nombre"
+                    autoComplete="off"
+                    value={ name }
+                    onChange= { handleInputChange } 
+                />
+            </div>
+            <div className="form-group">
+                <input
+                    type="text"
+                    name="email"
+                    className="form-class"
+                    placeholder="Tu correo"
+                    autoComplete="off"
+                    value={ email }
+                    onChange= { handleInputChange } 
+                />
+            </div>
+        </>
+    )
+}

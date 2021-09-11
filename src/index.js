7f816ca9d174b0e45fd9f8fde347c47db3526886
Carolina Vejar <vejarcarolina.95@gmail.com>
2021-09-11 19:33:06 -0300

    Clase 111 useFetch - CustomHook

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { HooksApp } from './HooksApp';
 
 ReactDOM.render(
   <div className="container">
-    <FormWithCustomHook />
+    <MultipleCustomHooks />
   </div>,
     
   document.getElementById('root')

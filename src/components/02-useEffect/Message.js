250d2cac9f565909d0de3c31705c74abec0e82b5
Carolina Vejar <vejarcarolina.95@gmail.com>
2021-09-10 22:14:36 -0300

    Clase 109 useEffect - precauciones

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,16 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
 
+    const [coors, setCoors] = useState({ x: 0, y: 0});
+
+    const  { x, y } = coors; 
+
     useEffect(() => {
+
+        const mouseMove = (e) => {
+            const coors = { x: e.x, y: e.y}
+            console.log(coors);
+            setCoors(coors);
+        }
+
         console.log(`Componente montado`);
+
+        window.addEventListener('mousemove', mouseMove);
+        
         return () => {
+            window.removeEventListener('mousemove', mouseMove)
             console.log(`Componente desmontado`);
         }
     }, [])
     return (
         <div>
             <h3>Eres genial uwu</h3>
+            <p>
+                x: {x}, y: {y}
+            </p>
         </div>
     )
 }

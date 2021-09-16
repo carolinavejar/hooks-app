1cea6b35600b370cdcc0d0fefac4b96fd6e7f248
Carolina Vejar <vejarcarolina.95@gmail.com>
2021-09-16 00:06:40 -0300

    Clase 125 Cascaron de lista ToDo

--- src/components/08-useReducer/ToDoApp.js
+++ src/components/08-useReducer/ToDoApp.js
@@ -13,14 +13,55 @@ export const ToDoApp = () => {
     const [ toDos] = useReducer(toDoReducer, initialState);
     console.log(toDos);
     return (
-        <div>
-            <h1>To Do App</h1>
+        <div id = "todoList">
+            <h1>To Do App ( {toDos.length} )</h1>
             <hr />
-            <ul>
-                <li>Hola 1</li>
-                <li>Hola 2</li>
-                <li>Hola 3</li>
-            </ul>
+
+            <div className = "row">
+                <div className="col-7">
+                    
+                    <ul>
+                        {
+                            toDos.map( (todo, i) => (
+                                <li
+                                    key = { todo.id }
+                                    className = "list-group-item"
+                                >
+                                    <small>{ i + 1 }. {todo.desc}</small>
+
+                                    <button 
+                                        className="btn btn-outline-primary ml-3" 
+                                        id = "btnBorrar"
+                                    >
+                                        Borrar
+                                    </button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>Agregar TO DO</h4>
+                    <hr />
+                    <form>
+                        <div className = "d-grid gap-2">
+                            <input
+                                className = "form-control"
+                                type = "text"
+                                name = "decription"
+                                placeholder = "Aprender ..."
+                                autoComplete = "off"
+                            />
+                            <button 
+                                className="btn btn-outline-primary btn-block mt-1" 
+                            >
+                                Agregar
+                            </button>
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }

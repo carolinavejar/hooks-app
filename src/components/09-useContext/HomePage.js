05e6f16481de0cf7f7d473a5acf6f5d60fcb6cdf
Carolina Vejar <vejarcarolina.95@gmail.com>
2021-09-30 22:06:17 -0300

    Clase 140 CreateContext y useContext

--- src/components/09-useContext/HomePage.js
+++ src/components/09-useContext/HomePage.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export const HomePage = () => {
+
+    const userContext = useContext(UserContext);
+    console.log(userContext);
     return (
         <div className="container">
             <h3>Home Pages</h3>
--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
+    const user = {
+        id:1234,
+        name: 'Carito',
+        email: 'vejarcarolina.95@gmail.com'
+    } 
     return (
-        <>
-            <AppRouter></AppRouter>
-        </>
+        <UserContext.Provider value = { user }>
+            <AppRouter />
+        </UserContext.Provider>
     )
 }
